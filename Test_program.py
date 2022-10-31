07add464f1a7d67367043f441b72129e2933fd22
SthitaprajnaPriyadarshan <sthitaprajna2014@gmail.com>
2022-10-31 07:46:18 +0530

    I/P data, added file etc

--- Test_program.py
+++ Test_program.py
@@ -2,3 +2,23 @@ a = 20
 b = 30
 print(a + b)
 print('Done')
+
+
+with open('inputPS15Q1.txt', 'r') as fi:
+	contents = fi.readlines();
+	print(contents, end='\n')
+	
+
+# print(contents)
+(d, e) = (20, 30)
+print(d, e)
+
+s = (a, b, d, e)
+print(s)
+
+def priii():
+	print('Hello')
+
+
+
+priii()
